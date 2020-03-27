2e2c1a699d920451d11c42894f75725fd4d5d172
Teng Liang <philoliang2011@gmail.com>
2020-03-26 22:12:56 -0700

    add different update algorithms

--- mt_simulator.py
+++ mt_simulator.py
@@ -1,6 +1,8 @@
 
 import collections
 
+PIT_THRESHOLD = 0
+
 class Node():
 	def __init__(self, value=None, level=0):
 		self._value = value
@@ -17,7 +19,7 @@ class Measurement():
 	"""ranking: pit_counter, child_counter"""
 
 	def __init__(self):
-		self.rank_counters = dict()
+		self.rank_counters = collections.defaultdict(lambda : [ 0, 0])
 
 	def __str__(self):
 		return self.rank_counters.__str__()
@@ -102,7 +104,7 @@ class NameTree():
 		current = self.insert_path(name)
 		current._fib = rank
 		current._meas = Measurement()
-		current._meas.rank_counters = (0,0)
+		# current._meas.rank_counters[rank] = [0,0]
 		# print(current)
 
 	def mt_update_fib(self, name, rank):
@@ -118,29 +120,164 @@ class NameTree():
 			next_fib_node._fib = rank
 
 	def mt_update_pit(self, name, rank):
-		pass
+		"""
+			find fib, check if ranks are the same,
+			if not add a fib at pit-1 level with the new rank
+		"""
+		fib_node = self.find_fib(name)
+		if fib_node._fib != rank:
+			pit_parent_name = "/".join(name.split("/")[:-1])
+			next_fib_node = self.insert_path(pit_parent_name)
+			next_fib_node._fib = rank
 
 	def mt_update_accurate(self, name, rank):
-		pass
+		"""
+			1. lookup FIB to find measurements
+			2. increase PIT counter, if it reaches PIT_THRESHOLD follow the following steps
+			3. traverse from FIB to name, insert measurements at each name component
+			4, update or insert FIB to the first name prefix that has only one ranking
+		"""
+		fib_node = self.find_fib(name)
+
+		rank_counters = fib_node._meas.rank_counters
+		rank_counters[rank][0] += 1 # increment pit counter
+
+		fib_inserted = None
+		if rank != fib_node._fib: # insert a fib only when a new ranking is found
+			fib_inserted = False
+		else:
+			fib_inserted = True
+
+		if rank_counters[rank][0] >= PIT_THRESHOLD:
+			# check if FIB update is needed at FIB level
+			if len(fib_node._meas.rank_counters) == 1 and not fib_inserted: # only one rank, insert fib
+				fib_node._fib = rank
+				print("Updating FIB rank: ", fib_node)
+				fib_inserted = True
+
+			# insert measurements from fib to pit
+			rest_componets = name.split('/')[fib_node._level+1:]
+			current = fib_node
+
+			for comp in rest_componets:
+				child_exists = False
+				for child in current._children:
+					if child._value == comp: #child exists
+						child_exists = True
+						child._meas.rank_counters[rank][0] += 1 # update pit counter
+
+						if len(child._meas.rank_counters) == 1 and not fib_inserted: # only one rank, insert fib
+							child._fib = rank
+							fib_inserted = True
+							print("Inserting FIB at node:", child)
+							self.mt_collapse_fib(child._parent)
+
+				if not child_exists:
+					#add a new node
+					child = Node(comp, current._level+1)
+					child._parent = current
+					child._meas = Measurement()
+					current._children.append(child)
+
+					child._meas.rank_counters[rank][0] += 1 # update measurement
+					if len(child._meas.rank_counters) == 1 and not fib_inserted: # only one rank, insert fib
+						child._fib = rank
+						fib_inserted = True
+						print("Inserting FIB at node:", child)
+						self.mt_collapse_fib(child._parent)
+
+				current = child
+
+	def mt_collapse_fib(self, node):
+		"""
+			when a new fib is updated or inserted, check if fib collapse is needed
+			node is the parent of newly inserted FIB
+			assuming two ranks
+			this node should contain fib
+		"""
+		if node._fib == None:
+			return
+
+		child_counter = {"r1":0,"r2":0}
+		for child in node._children:
+			if child._fib != None:
+				child_counter[child._fib] += 1
+			else:
+				child_counter[node._fib] += 1
+
+		if child_counter['r1'] != child_counter['r2'] and \
+			node._fib != max(child_counter['r1'], child_counter['r2']):
+			print("start collapsing, because of", child_counter)
+			for child in node._children:
+				if child._fib == None: # add fib
+					child._fib = node._fib
+					print("adding fib to node", child)
+				else:
+					child._fib = None
+					print("deleting fib to node", child)
+
+			current = node._parent
+			while current:
+				if current._fib == node._fib:
+					node._fib = None
+					print("deleting fib at the collapsing node")
+				current = current._parent
+
 
 
 if __name__ == "__main__":
 	#test basics
 	nt = NameTree()
-	nt.insert_fib("/a/b","r1")
+	nt.insert_fib("/a","r1")
 	print(nt)
-	print("FIB count: ", nt.count_fib())
-	print(nt.find_fib("/a/b/c1/1"))
+	print("FIB count:", nt.count_fib())
+	# print("FIB node:", nt.find_fib("/a/b/c1/1"))
 
 	#test mt_update_fib
-	nt.mt_update_fib("/a/b/c1/d1/1", "r2")
-	print(nt)
-	print("FIB count: ", nt.count_fib())
+	# nt.mt_update_fib("/a/b/c1/d1/1", "r2")
+	# print(nt)
+	# print("FIB count: ", nt.count_fib())
 
-	nt.mt_update_fib("/a/b/c1/d2/1", "r2")
-	print(nt)
-	print("FIB count: ", nt.count_fib())
+	# nt.mt_update_fib("/a/b/c1/d2/1", "r2")
+	# print(nt)
+	# print("FIB count: ", nt.count_fib())
 
-	nt.mt_update_fib("/a/b/c1/d2/1", "r1")
-	print(nt)
-	print("FIB count: ", nt.count_fib())
+	# nt.mt_update_fib("/a/b/c1/d2/1", "r1")
+	# print(nt)
+	# print("FIB count: ", nt.count_fib())
+
+	#test mt_update_pit
+	# print("\nmt_update_pit testing...")
+	# nt.mt_update_pit("/a/b/c1/d1/1", "r1")
+	# print(nt)
+	# print("FIB count: ", nt.count_fib())
+
+	# nt.mt_update_pit("/a/b/c1/d1/1", "r2")
+	# print(nt)
+	# print("FIB count: ", nt.count_fib())
+
+	# nt.mt_update_pit("/a/b/c1/d2/1", "r1")
+	# print(nt)
+	# print("FIB count: ", nt.count_fib())
+
+	# nt.mt_update_pit("/a/b/c1/d2/1", "r2")
+	# print(nt)
+	# print("FIB count: ", nt.count_fib())
+
+	#test mt_update_accurate
+	nt.mt_update_accurate("/a/b/c1/d1/1", "r2")
+	# print(nt)
+	# print("FIB node:", nt.find_fib("/a/b/c1/d2/1"))
+	# print("FIB count: ", nt.count_fib())
+
+	nt.mt_update_accurate("/a/b/c2/d1/1", "r1")
+	# print(nt)
+	# print("FIB node:", nt.find_fib("/a/b/c2/d1/1"))
+	# print("FIB count: ", nt.count_fib())
+
+	# nt.mt_update_accurate("/a/b2/c1/d1/1", "r1")
+	# print(nt)
+	# print("FIB node:", nt.find_fib("/a/b2/c1/d1/1"))
+	# print("FIB count: ", nt.count_fib())
+
+	nt.mt_update_accurate("/a/b/c3/d1/1", "r1")
